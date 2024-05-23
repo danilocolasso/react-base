00bdc66c7bce10e28a6a29be49b4b7c89f773c83
danilocolasso <danilo.colasso@meisters.solutions>
2024-05-22 23:13:36 -0300

    Add debounceTime parameter to AsyncSelect

--- src/components/AsyncSelect/AsyncSelect.tsx
+++ src/components/AsyncSelect/AsyncSelect.tsx
@@ -7,18 +7,21 @@ export interface AsyncSelectProps extends Omit<SelectProps, 'options'> {
   service: (payload: any) => Promise<PaginatedResponse<any>>;
   map?: (options: any[]) => any[];
   autofetch?: boolean;
+  debounceTime?: number;
 }
 
 export const AsyncSelect = React.forwardRef<HTMLDivElement, AsyncSelectProps>(({
   service,
   map,
   autofetch,
+  debounceTime,
   ...props
 }, ref) => {
   const { options, loading, handleSearchChange } = useAsyncSelect<SelectOption>(
     service,
     map,
     autofetch,
+    debounceTime
   );
 
   return (
